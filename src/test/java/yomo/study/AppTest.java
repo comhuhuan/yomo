7f806882e484a47a5e84713a884cbb7db714ad8d
movescar@163.com <huhuan66612>
2018-07-27 15:48:17 +0800

    init

---
+++ src/test/java/yomo/study/AppTest.java
@@ -0,0 +1,20 @@
+package yomo.study;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
